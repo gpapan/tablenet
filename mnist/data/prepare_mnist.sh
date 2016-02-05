bdc02e329d2c53725a3581490e30270fff1b8f07
Iasonas <iasonas.kokkinos@gmail.com>
2016-02-05 17:51:46 +0100

    running first experiment - code seems to work

--- mnist/data/prepare_mnist.sh
+++ mnist/data/prepare_mnist.sh
@@ -5,12 +5,14 @@ CAFFE_DIR=${HOME}/rmt/work/deeplabel/caffe_google
 # Get MNIST data and build LMDB files if not already there
 
 DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+echo $DIR
 cd $DIR
 
 if [ ! -d mnist_train_lmdb ]; then
-    rm -rf mnist_{train,test}_lmdb
-    rm -f {train,t10k}-*
+    #rm -rf mnist_{train,test}_lmdb
+    #rm -f {train,t10k}-*
     ${CAFFE_DIR}/data/mnist/get_mnist.sh
+    
     mv ${CAFFE_DIR}/data/mnist/{train,t10k}-* .
     CONVERT_BIN=${CAFFE_DIR}/.build_release/examples/mnist/convert_mnist_data.bin
     ${CONVERT_BIN} \
